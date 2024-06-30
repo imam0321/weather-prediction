20341f6a455bc56db629697ce1059c422074fcc8
imam0321 <imam.hossain0321@gmail.com>
2024-06-30 22:08:28 +0600

    added UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/header/Header";
+import WeatherBoard from "./components/weather/WeatherBoard.jsx";
 
 function App() {
-
   return (
-    <>
-      <h1>weather Prediction</h1>
-    </>
-  )
+    <div className="grid place-items-center h-screen">
+      <Header />
+      <main>
+        <section className="lg:mt-14">
+          <WeatherBoard/>
+        </section>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
